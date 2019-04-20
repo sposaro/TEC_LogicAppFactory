7a06aaaed6fec67c30db913611b39f91be39cd75
Frank Sposaro <franksp@microsoft.com>
2019-04-19 17:23:52 -0700

    Adding templates to create and edit logic app

---
+++ source/TecDeploymentHelper.cs
@@ -0,0 +1,135 @@
+// Requires the following Azure NuGet packages and related dependencies:
+// package id="Microsoft.Azure.Management.Authorization" version="2.0.0"
+// package id="Microsoft.Azure.Management.ResourceManager" version="1.4.0-preview"
+// package id="Microsoft.Rest.ClientRuntime.Azure.Authentication" version="2.2.8-preview"
+  
+using Microsoft.Azure.Management.ResourceManager;
+using Microsoft.Azure.Management.ResourceManager.Models;
+using Microsoft.Rest.Azure.Authentication;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net.Http;
+
+namespace TecLogicAppLibrary
+{
+    /// <summary>
+    /// This is a helper class for deploying an Azure Resource Manager template
+    /// More info about template deployments can be found here https://go.microsoft.com/fwLink/?LinkID=733371
+    /// </summary>
+    class DeploymentHelper
+    {
+        public async void Run(string subscriptionId,
+                              string clientId,
+                              string clientSecret,
+                              string resourceGroupName,
+                              string deploymentName,
+                              string resourceGroupLocation, // must be specified for creating a new resource group
+                              string pathToTemplateFile,
+                              string pathToParameterFile,
+                              string tenantId)
+        {
+            // Try to obtain the service credentials
+            var serviceCreds = await ApplicationTokenProvider.LoginSilentAsync(tenantId, clientId, clientSecret);
+
+            // Read the template and parameter file contents
+            JObject templateFileContents = GetJsonFileContents(pathToTemplateFile);
+            JObject parameterFileContents = GetJsonFileContents(pathToParameterFile);
+
+            // Create the resource manager client
+            var resourceManagementClient = new ResourceManagementClient(serviceCreds);
+            resourceManagementClient.SubscriptionId = subscriptionId;
+
+            // Create or check that resource group exists
+            EnsureResourceGroupExists(resourceManagementClient, resourceGroupName, resourceGroupLocation);
+
+            // Start a deployment
+            DeployTemplate(resourceManagementClient, resourceGroupName, deploymentName, templateFileContents, parameterFileContents);
+        }
+
+        /// <summary>
+        /// Creates an endpoint to trigger a logic app
+        /// </summary>
+        /// <returns>The url endpoint what was created to trigger the specified logic app resource</returns>
+        public string async CreateLogicAppHttpTrigger(string logicAppResourceId, string myEndPointTrigger)
+        {
+            string createUrl = @"https://management.azure.com/{logicAppResourceID}/triggers/{myEndPointTrigger}/listCallbackURL?api-version=2016-06-01";
+        
+            var values = new Dictionary<string, string>
+            {
+                { "thing1", "hello" },
+                { "thing2", "world" }
+            };
+
+            var content = new FormUrlEncodedContent(values);
+            var response = await client.PostAsync(createUrl, content);
+            var responseString = await response.Content.ReadAsStringAsync();
+        }
+
+        private static readonly HttpClient client = new HttpClient();
+
+        /// <summary>
+        /// Reads a JSON file from the specified path
+        /// </summary>
+        /// <param name="pathToJson">The full path to the JSON file</param>
+        /// <returns>The JSON file contents</returns>
+        private JObject GetJsonFileContents(string pathToJson)
+        {
+            JObject templatefileContent = new JObject();
+            using (StreamReader file = File.OpenText(pathToJson))
+            {
+                using (JsonTextReader reader = new JsonTextReader(file))
+                {
+                    templatefileContent = (JObject)JToken.ReadFrom(reader);
+                    return templatefileContent;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Ensures that a resource group with the specified name exists. If it does not, will attempt to create one.
+        /// </summary>
+        /// <param name="resourceManagementClient">The resource manager client.</param>
+        /// <param name="resourceGroupName">The name of the resource group.</param>
+        /// <param name="resourceGroupLocation">The resource group location. Required when creating a new resource group.</param>
+        private static void EnsureResourceGroupExists(ResourceManagementClient resourceManagementClient, string resourceGroupName, string resourceGroupLocation)
+        {
+            if (resourceManagementClient.ResourceGroups.CheckExistence(resourceGroupName) != true)
+            {
+                Console.WriteLine(string.Format("Creating resource group '{0}' in location '{1}'", resourceGroupName, resourceGroupLocation));
+                var resourceGroup = new ResourceGroup();
+                resourceGroup.Location = resourceGroupLocation;
+                resourceManagementClient.ResourceGroups.CreateOrUpdate(resourceGroupName, resourceGroup);
+            }
+            else
+            {
+                Console.WriteLine(string.Format("Using existing resource group '{0}'", resourceGroupName));
+            }
+        }
+
+        /// <summary>
+        /// Starts a template deployment.
+        /// </summary>
+        /// <param name="resourceManagementClient">The resource manager client.</param>
+        /// <param name="resourceGroupName">The name of the resource group.</param>
+        /// <param name="deploymentName">The name of the deployment.</param>
+        /// <param name="templateFileContents">The template file contents.</param>
+        /// <param name="parameterFileContents">The parameter file contents.</param>
+        private static void DeployTemplate(ResourceManagementClient resourceManagementClient, string resourceGroupName, string deploymentName, JObject templateFileContents, JObject parameterFileContents)
+        {
+            Console.WriteLine(string.Format("Starting template deployment '{0}' in resource group '{1}'", deploymentName, resourceGroupName));
+            var deployment = new Deployment();
+
+            deployment.Properties = new DeploymentProperties
+            {
+                Mode = DeploymentMode.Incremental,
+                Template = templateFileContents,
+                Parameters = parameterFileContents["parameters"].ToObject<JObject>()
+            };
+
+            var deploymentResult = resourceManagementClient.Deployments.CreateOrUpdate(resourceGroupName, deploymentName, deployment);
+            Console.WriteLine(string.Format("Deployment status: {0}", deploymentResult.Properties.ProvisioningState));
+        }
+    }
+}
